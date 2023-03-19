f6d00b2e648d26b1c17c8763d7d94339f98f6292
Dawon Lee <lid09270411@gmail.com>
2023-03-19 15:10:25 +0900

    Add files via upload

---
+++ comments.c
@@ -0,0 +1,24 @@
+// comments.c
+// write comments in your C program
+// to make your code understandable to people
+// explain what is going on
+// add comments as you write your code
+// you can quickly scan through your code and comments to modify your program
+// C ignores comments
+                                                            // single line comments
+/* DW programming */
+/* calculate how many hours are in a month */
+                                                            /* multi line comments*/
+#include <stdio.h>
+main()
+{
+    int numOfDaysInMonth, numOfHoursInMonth, numOfHoursInDay = 24;
+    printf("How many days are in a month? ");
+    scanf(" %d", &numOfDaysInMonth);
+
+    numOfHoursInMonth = numOfDaysInMonth * numOfHoursInDay; // calculate the total hours in a month
+
+    printf("Hours in a month : %d", numOfHoursInMonth);
+
+    return 0;
+}
